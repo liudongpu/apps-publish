cee9d4e3067d20b1f0d21b3a42b2cb4000f8f6dc
liudongpu <liudongpu@hotmail.com>
2020-03-12 14:56:27 +0800

    add delete script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,10 @@
 
+echo 'delete'
+
 #删除两个月前的android
 find /opt/fabu-data/upload -mtime +60 -type f -name "*.apk" | xargs rm -rf;
+
+
 
 #删除两个月前的ios
 find /opt/fabu-data/upload -mtime +60 -type f -name "*.ipa" | xargs rm -rf;
